e762c6f81d3abd662d71429625a16f1184bf928b
Alexandre Varas <alej0varas@gmail.com>
2018-07-26 13:02:06 +0200

    Made URL slash optional

--- djabberd/urls.py
+++ djabberd/urls.py
@@ -6,8 +6,8 @@ from . import views
 app_name = 'djabberd'
 
 urlpatterns = [
-    url(r'^auth/$', views.AuthView.as_view(), name='auth'),
-    url(r'^user/$', views.UserView.as_view(), name='user'),
-    url(r'^roster/$', views.RosterView.as_view(), name='roster'),
-    url(r'^archive/$', views.ArchiveView.as_view(), name='archive'),
+    url(r'^auth/?$', views.AuthView.as_view(), name='auth'),
+    url(r'^user/?$', views.UserView.as_view(), name='user'),
+    url(r'^roster/?$', views.RosterView.as_view(), name='roster'),
+    url(r'^archive/?$', views.ArchiveView.as_view(), name='archive'),
 ]
