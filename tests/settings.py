17fd51ce77579dfd191dae272e02a345a4eb8851
Alexandre Varas <alej0varas@gmail.com>
2020-05-29 15:01:38 +0200

    Removed "admin" from test settings
    It's not required by djabberd and makes Django system check to fail

--- tests/settings.py
+++ tests/settings.py
@@ -9,7 +9,6 @@ SECRET_KEY = 'fake-key'
 INSTALLED_APPS = ('django.contrib.auth',
                   'django.contrib.contenttypes',
                   'django.contrib.sessions',
-                  'django.contrib.admin',
                   'django.contrib.sites',
                   'tests', )
 
