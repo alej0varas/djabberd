cf5f46718dad5cce57b00663d9285373e9e714cc
Alexandre Varas <alej0varas@gmail.com>
2020-05-29 15:03:28 +0200

    Updated "reverse" import
    Module "django.core.urlresolvers" was deprecated

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,6 +1,6 @@
 from unittest import mock
 
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 
 from rest_framework import status
 from rest_framework.test import APIRequestFactory
